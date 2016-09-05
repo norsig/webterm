30f71dc8ae2a868a54e6dca7004df4b10c70b9f0
Northern Signal <jswanson@northernsignal.com>
2016-09-05 17:31:02 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
         },
         shell: {
             build_hterm: {
-                command: 'LIBDOT_SEARCH_PATH=$(pwd) ./libdot/bin/concat.sh -i ./hterm/concat/hterm_all.concat -o ../../public/wetty/hterm_all.js',
+                command: 'LIBDOT_SEARCH_PATH=$(pwd) ./libdot/bin/concat.sh -i ./hterm/concat/hterm_all.concat -o ../../public/webterm/hterm_all.js',
                 options: {
                     execOptions: {
                         cwd: './tmp/libapps'
